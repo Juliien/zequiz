1770aa57d15c064418772e3919b64832ac5e1efd
Juliien <julien1105@outlook.fr>
2021-02-19 03:20:37 +0100

    refacto config

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const app = require('./api/index');
 const express = require('express');
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist/zequiz'));
+app.use(express.static(path.join(__dirname + '/dist/')));
 
 
