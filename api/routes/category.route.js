8725fcbb2ea71467131a810738f9738ed32b1303
Juliien <julien1105@outlook.fr>
2021-02-19 02:34:53 +0100

    fix env

--- api/routes/category.route.js
+++ api/routes/category.route.js
@@ -1,7 +1,7 @@
 const bodyParser = require('body-parser');
 
 module.exports = function (app) {
-  app.get(process.env.API_URL + '/', bodyParser.json(),  async (req, res) => {
+  app.get(process.env.API_URL + '/test', bodyParser.json(),  async (req, res) => {
     return res.status(200).json({message: 'api v1'});
   });
 };
