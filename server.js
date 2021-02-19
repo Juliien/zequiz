5fb5668cefbe0a09c09b8536bc02434e45048cd1
Juliien <julien1105@outlook.fr>
2021-02-19 02:58:23 +0100

    serv

--- server.js
+++ server.js
@@ -3,7 +3,5 @@ const express = require('express');
 const path = require('path');
 
 app.use(express.static(__dirname + '/dist/zequiz'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/zequiz/index.html'));
-});
+
 
