3e5aef964ac677e5788e4eed25ed814c333db3a6
Juliien <julien1105@outlook.fr>
2021-02-27 02:07:09 +0100

    mobile view menu

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -11,23 +11,37 @@ export class HeaderComponent implements OnInit {
   isMobile: boolean;
   code: number;
   error: boolean;
+  active: number;
 
-  constructor(private roomService: RoomService,
-              private router: Router) { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     this.error = false;
     this.isMobile = window.innerWidth <= 765;
+    if (document.location.href === 'https://www.zequiz.net/#/home') {
+      this.active = 0;
+    } else if (document.location.href === 'https://www.zequiz.net/#/all') {
+      this.active = 0;
+    } else {
+      this.active = -1;
+    }
  }
 
  clear() {
    sessionStorage.clear();
  }
-  verifyCode() {
-    this.roomService.joinRoom(this.code).subscribe(room => {
-        sessionStorage.setItem('roomId', room._id);
-        this.router.navigate(['/room', room._id]).then();
-    });
-  }
 
+ goToCategories() {
+    this.clear();
+    this.active = 1;
+    console.log(this.active);
+    this.router.navigate(['/all']).then();
+ }
+
+  goToHome() {
+    this.clear();
+    this.active = 0;
+    console.log(this.active);
+    this.router.navigate(['/home']).then();
+  }
 }
