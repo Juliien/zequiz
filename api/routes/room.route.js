9b2b9ca9454e2f0cddc933fe7b86ef3a36a96e3d
Juliien <julien1105@outlook.fr>
2021-03-01 18:39:36 +0100

    back room

--- api/routes/room.route.js
+++ api/routes/room.route.js
@@ -8,5 +8,5 @@ module.exports = function (app) {
   app.get(process.env.API_URL + '/room/:id', bodyParser.json(), async (req, res) => controller.getRoomById(req, res));
   app.post(process.env.API_URL + '/room/join/:id', bodyParser.json(),async (req, res) => controller.joinRoom(req, res));
   app.post(process.env.API_URL + '/room/close/:id', bodyParser.json(), async (req, res) => controller.closeRoom(req, res));
-  app.delete(process.env.API_URL + '/room/:key', bodyParser.json(), async (req, res) => controller.purgeRoom(req, res));
+  app.delete(process.env.API_URL + '/room/:key', async (req, res) => controller.purgeRoom(req, res));
 };
