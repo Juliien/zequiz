edd06a6c95c3681be9847d7e4e358fca5dcc8f26
Juliien <julien1105@outlook.fr>
2021-03-20 13:54:44 +0100

    fixed game validation

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -7,96 +7,96 @@ import {RoomModel} from '../../models/room.model';
 import {environment} from '../../../environments/environment';
 
 @Component({
-  selector: 'app-game',
-  templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+    selector: 'app-game',
+    templateUrl: './game.component.html',
+    styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
-  @Input() quiz: any;
-  @Input() vs: boolean;
-  @Input() room: RoomModel;
+    @Input() quiz: any;
+    @Input() vs: boolean;
+    @Input() room: RoomModel;
 
-  index: number;
-  score: number;
-  result = false;
-  answer = false;
-  listQuestions: QuizModel[] = [];
-  selectedAnswer: string;
-  correctAnswer: string;
-  errMsg: string;
-  socket: any;
+    index: number;
+    score: number;
+    result = false;
+    answer = false;
+    listQuestions: QuizModel[] = [];
+    selectedAnswer: string;
+    correctAnswer: string;
+    errMsg: string;
+    socket: any;
 
-  constructor(private router: Router,
-              private playerService: PlayerService) { }
+    constructor(private router: Router,
+                private playerService: PlayerService) { }
 
-  ngOnInit() {
-    this.socket = io(environment.socketUrl);
-    this.startQuiz();
-  }
-
-  startQuiz() {
-    this.listQuestions = this.quiz.results;
-    this.index = 0;
-    this.score = 0;
-  }
-
-  parseQuestion(res: string) {
-    return res.replace(/&apos;/g, '\'')
-        .replace(/&quot;/g, '"')
-        .replace(/&gt;/g, '>')
-        .replace(/&lt;/g, '<')
-        .replace(/&amp;/g, '&')
-        .replace(/&deg;/g, '°')
-        .replace(/&#039;/g, '\'')
-        .replace(/&rsquo;/g, '\'')
-        .replace(/&eacute;/g, 'é')
-        .replace(/&agrave;/g, 'à')
-        .replace(/&aelig;/g, 'æ')
-        .replace(/&egrave;/g, 'è')
-        .replace(/&ecirc;/g, 'ê')
-        .replace(/&epsilon;/g, 'ε')
-        .replace(/&Phi;/g, 'Φ')
-        .replace(/&phi;/g, 'φ')
-        .replace(/&ldquo;/g, '"')
-        .replace(/&rdquo;/g, '"')
-        .replace(/&ocirc;/g, 'ô');
-  }
-
-  nextQuestion() {
-    this.index += 1;
-    this.answer = false;
-  }
-
-  validate(res: string) {
-    this.correctAnswer = this.listQuestions[this.index].correct_answer.toUpperCase();
-    this.selectedAnswer = res.toUpperCase();
-    if (this.selectedAnswer === this.correctAnswer) {
-      this.score++;
+    ngOnInit() {
+        this.socket = io(environment.socketUrl);
+        this.startQuiz();
     }
-    this.answer = true;
-  }
 
-  displayResult() {
-    this.result = true;
-  }
+    startQuiz() {
+        this.listQuestions = this.quiz.results;
+        this.index = 0;
+        this.score = 0;
+    }
+
+    parseQuestion(res: string) {
+        return res.replace(/&apos;/g, '\'')
+            .replace(/&quot;/g, '"')
+            .replace(/&gt;/g, '>')
+            .replace(/&lt;/g, '<')
+            .replace(/&amp;/g, '&')
+            .replace(/&deg;/g, '°')
+            .replace(/&#039;/g, '\'')
+            .replace(/&rsquo;/g, '\'')
+            .replace(/&eacute;/g, 'é')
+            .replace(/&agrave;/g, 'à')
+            .replace(/&aelig;/g, 'æ')
+            .replace(/&egrave;/g, 'è')
+            .replace(/&ecirc;/g, 'ê')
+            .replace(/&epsilon;/g, 'ε')
+            .replace(/&Phi;/g, 'Φ')
+            .replace(/&phi;/g, 'φ')
+            .replace(/&ldquo;/g, '"')
+            .replace(/&rdquo;/g, '"')
+            .replace(/&ocirc;/g, 'ô');
+    }
+
+    nextQuestion() {
+        this.index += 1;
+        this.answer = false;
+    }
+
+    validate(res: string) {
+        this.correctAnswer = this.listQuestions[this.index].correct_answer.toUpperCase();
+        this.selectedAnswer = res.toUpperCase();
+        if (this.selectedAnswer === this.correctAnswer) {
+            this.score++;
+        }
+        this.answer = true;
+    }
+
+    displayResult() {
+        this.result = true;
+    }
 
 
-  displayVSResult() {
-    this.result = true;
-    const data =  {
-      playerId: sessionStorage.getItem('playerId'),
-      score: this.score
-    };
-    this.playerService.updatePlayerScore(data).subscribe( () => this.socket.emit('room', 'finished'));
-  }
+    displayVSResult() {
+        this.result = true;
+        const data =  {
+            playerId: sessionStorage.getItem('playerId'),
+            score: this.score
+        };
+        this.playerService.updatePlayerScore(data).subscribe( () => this.socket.emit('room', 'finished'));
+    }
 
 
-  goToCategories() {
-    sessionStorage.clear();
-    this.router.navigate(['all']).then();
-  }
+    goToCategories() {
+        sessionStorage.clear();
+        this.router.navigate(['all']).then();
+    }
 
-  restart() {
-    window.location.reload();
-  }
+    restart() {
+        window.location.reload();
+    }
 }
